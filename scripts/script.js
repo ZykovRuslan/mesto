29b53d6ebed87e4526b5e29343f6436c27f952c1
Ruslan Zykov <r.v.zykov@rambler.ru>
2023-01-11 22:52:09 +0400

    fix: исправлены замечания после 1 итерации

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 let page = document.querySelector('.page');
 let content = document.querySelector('.content');
-let popup = content.querySelector('.popup');
-let closeButton = content.querySelector('.popup__close-button');
-let editButton = content.querySelector('.profile__edit-button');
-let submitButton = content.querySelector('.popup__submit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+let editButton = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__name');
 let profileAboutMe = document.querySelector('.profile__about-me');
 let formElement = document.querySelector('.popup__container');
@@ -11,31 +10,18 @@ let nameInput = document.querySelector('.popup__input_data_name');
 let jobInput = document.querySelector('.popup__input_data_job');
 
 //ФУНКЦИЯ ДЛЯ ВЫЗОВА ВСПЛЫВАЮЩЕГО ОКНА
-function addProfile() {
-  //Присвоение нового класса (модификатор) popup_opened блоку popup через метод .classList.add
-  //В CSS для класса добавляем display: block;
-  popup.classList.add('popup_opened');
-  let nameInput = document.querySelector('.popup__input_data_name');
-  let jobInput = document.querySelector('.popup__input_data_job');
+function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileAboutMe.textContent;
-  //console.log(nameInput.value);
+  //Присвоение нового класса (модификатор) popup_opened блоку popup через метод .classList.add
+  //В CSS для класса добавляем display: flex;
+  popup.classList.add('popup_opened');
 }
 
 //ФУНКЦИЯ ДЛЯ ЗАКРЫТИЯ ВСПЛЫВАЮЩЕГО ОКНА ПО КНОПКЕ "СОХРАНИТЬ"
-function saveProfile() {
+function closePopup() {
   //Для переменной popup используем удаление класса элементу через метод .classList.remove как аргумент передаем методу класс 'popup_opened'
   popup.classList.remove('popup_opened');
-}
-
-//ФУНКЦИЯ ДЛЯ ЗАКРЫТИЯ ВСПЛЫВАЮЩЕГО ОКНА ПО КНОПКЕ "ЗАКРЫТЬ"
-function closeProfile() {
-  //Для переменной popup используем удаление класса элементу через метод .classList.remove как аргумент передаем методу класс 'popup_opened'
-  popup.classList.remove('popup_opened');
-  let nameInput = document.querySelector('.popup__input_data_name');
-  let jobInput = document.querySelector('.popup__input_data_job');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileAboutMe.textContent;
 }
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
@@ -45,23 +31,21 @@ function handleFormSubmit(evt) {
   // О том, как это делать, расскажем позже.
 
   // Получите значение полей jobInput и nameInput из свойства value
-  //console.log(evt);
   let job = jobInput.value;
   let name = nameInput.value;
-  // Выберите элементы, куда должны быть вставлены значения полей
-  profileName = document.querySelector('.profile__name');
-  profileAboutMe = document.querySelector('.profile__about-me');
 
   // Вставьте новые значения с помощью textContent
   profileName.textContent = name;
   profileAboutMe.textContent = job;
+
+  closePopup();
 }
 // Прикрепляем обработчик к форме:
 // он будет следить за событием 'submit' - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
 
 //реакция на действия пользователя (редактировать) посредством обрабочика события методом addEventListener
-//(click - тип события, addProfile - функция обработчик)
-editButton.addEventListener('click', addProfile);
-submitButton.addEventListener('click', saveProfile);
-closeButton.addEventListener('click', closeProfile);
+//(click - тип события, openPopup - функция обработчик)
+editButton.addEventListener('click', openPopup);
+formElement.addEventListener('submit', closePopup);
+closeButton.addEventListener('click', closePopup);
